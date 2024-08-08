fa29cf19e93e807248d9f8a7654fd280453f065c
Elsayed Mohamed <105808002+Sayed73@users.noreply.github.com>
2024-08-08 18:47:08 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,155 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+# Load the saved model
+model_periapical = pickle.load(open('Periapical_Diagnosis_Prediction.sav', 'rb'))
+model_pulpal = pickle.load(open('pulpal_Diagnosis_Prediction.sav', 'rb'))
+
+st.markdown("""
+    <style>
+    .stButton button {
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+    }
+    .stButton button:hover {
+        background-color: white;
+        color: black;
+        border: 2px solid #4CAF50;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
+def main():
+    st.title('DIAGNOSIS PULPAL AND PERIAPICAL DISEASES')
+
+    # Create four columns
+    col1, col2, col3= st.columns([2, 3, 3])
+
+    # Column 1 for general features
+    with col1:
+        st.markdown('<p style="font-size:26px; font-weight:bold;">A. Pain</p>', unsafe_allow_html=True)
+        pain_score = st.number_input('Pain Score', min_value=0, max_value=10)
+        Painkiller_usage = st.selectbox('Have you taken pain killers ?', ['Yes', 'No'])
+        Pain_duration = st.selectbox('When did the pain start ?', ['less than 2 days', 'more than 2 days', 'NA'])
+        Pulp_Vitality = st.selectbox('Pulp Vitality test(Cold)', ['+ve', '-ve', 'NA'])
+        affected_tooth = st.selectbox('Can you point out the offending tooth ?', ['Yes', 'No', 'NA'])
+        
+    # Convert the input strings to numeric values
+    Painkiller_usage = 0 if Painkiller_usage == 'No' else 1
+    Pain_duration = 0 if Pain_duration == 'more than 2 days' else 1 if Pain_duration == 'less than 2 days' else 99
+    Pulp_Vitality = 0 if Pulp_Vitality == '-ve' else 1 if Pulp_Vitality == '+ve' else 99
+    affected_tooth = 0 if affected_tooth == 'No' else 1 if affected_tooth == 'Yes' else 99
+
+    # Column 2 for additional general features
+    with col2:
+        # Center the "B. Clinical Examination" header
+        st.markdown('<p style="text-align:center; font-size:26px; font-weight:bold;">B. Clinical Examination</p>', unsafe_allow_html=True)
+
+        # Separating the features into two sub-columns
+        sub_col1, sub_col2 = st.columns(2)
+        with sub_col1:
+            swelling_eo = st.checkbox('Extra-Oral Swelling')
+            swelling_io = st.checkbox('Intra-Oral Swelling')
+            sinus_tract = st.checkbox('Sinus Tract')
+            tooth_open_history = st.selectbox('Was the tooth open Earlier ?', ['Yes', 'No', 'NA'])
+        with sub_col2:
+            palpation = st.checkbox('Palpation')
+            percussion = st.checkbox('Percussion')
+            mobility = st.checkbox('Mobility')
+        
+    # Convert the input strings to numeric values
+    tooth_open_history = 0 if tooth_open_history == 'No' else 1 if tooth_open_history == 'Yes' else 99
+    
+
+    # Column 3 for PAI features
+    with col3:
+        st.markdown('<p style="text-align:center; font-size:26px; font-weight:bold;">C. Radiographic Examination</p>', unsafe_allow_html=True)
+        # Separating the features into two sub-columns
+        sub_col3, sub_col4 = st.columns(2)
+        with sub_col3:
+            show_pai_options = st.selectbox('Is there a Periapical Radiolucency ?', ['No', 'Yes'])
+
+            # Initialize PAI features
+            pai_features = [0] * 5
+
+            if show_pai_options == 'Yes':
+                pai = st.radio('PAI Score', ['PAI 3', 'PAI 4', 'PAI 5'])
+                pai_features = [1 if pai == f else 0 for f in ['PAI 1', 'PAI 2','PAI 3', 'PAI 4', 'PAI 5']]
+
+            elif show_pai_options == 'No':
+                pai = st.radio('PAI Score', ['PAI 1', 'PAI 2'])
+                pai_features = [1 if pai == f else 0 for f in ['PAI 1', 'PAI 2','PAI 3', 'PAI 4', 'PAI 5']]
+        with sub_col4:
+            is_rcf = st.selectbox('Is there a previous Root Canal Filling ?', ['No','Yes'])
+    
+            if is_rcf == 'No':
+                acceptability = 99
+            else:
+                # If Yes, show the second question
+                quality_of_acceptability = st.selectbox('Is it Acceptable Root Canal Filling ?', ['Yes', 'No'])
+                
+                if quality_of_acceptability == 'Yes':
+                    acceptability = 1
+                else:
+                    acceptability = 0
+
+
+
+    # Predict when button is clicked
+    if st.button('Diagnose'):
+        features = [
+            pain_score, Painkiller_usage, Pain_duration, affected_tooth, tooth_open_history, palpation, percussion, mobility, *pai_features, 
+            acceptability, swelling_eo, swelling_io, sinus_tract, Pulp_Vitality
+        ]
+
+
+        # Predict periapical diagnosis
+        prediction_periapical = model_periapical.predict([features])[0]
+
+        if prediction_periapical == 0:
+            predicted_class_periapical = 'Acute apical abscess'
+        elif prediction_periapical == 1:
+            predicted_class_periapical = 'Asymptomatic apical periodontitis'
+        elif prediction_periapical == 2:
+            predicted_class_periapical = 'Chronic apical abscess'
+        elif prediction_periapical == 3:
+            predicted_class_periapical = 'Normal periapical tissues'
+        else:
+            predicted_class_periapical = 'Symptomatic apical periodontitis'
+
+        # Predict pulpal diagnosis
+        prediction_pulpal = model_pulpal.predict([features])[0]
+
+        if prediction_pulpal == 0:
+            predicted_class_pulpal = 'Asymptomatic irreversible pulpitis'
+        elif prediction_pulpal == 1:
+            predicted_class_pulpal = 'Necrotic pulp'
+        elif prediction_pulpal == 2:
+            predicted_class_pulpal = 'Previously initiated therapy'
+        elif prediction_pulpal == 3:
+            predicted_class_pulpal = 'Previously treated tooth'
+        elif prediction_pulpal == 4:
+            predicted_class_pulpal = 'Symptomatic irreversible pulpitis'
+        else:
+            predicted_class_pulpal = 'reversible pulpitis'
+
+        # Display predicted class and probabilities
+        # st.markdown(f'<p style="font-size:24px; color:black; font-weight:bold;">Diagnosis :</p>', unsafe_allow_html=True)
+        st.markdown(f'<p style="font-size:22px; color:#2e6c80;"><strong>Pulpal diagnosis:</strong> {predicted_class_pulpal}  </p>', unsafe_allow_html=True)
+        st.markdown(f'<p style="font-size:22px; color:#2e6c80;"><strong>Periapical diagnosis:</strong> {predicted_class_periapical}  </p>', unsafe_allow_html=True)
+        
+
+if __name__ == "__main__":
+    main()
