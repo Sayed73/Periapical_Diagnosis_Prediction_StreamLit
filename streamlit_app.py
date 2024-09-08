6e038d58043e7cc35c8ac645c66637babdfd45b0
Elsayed Mohamed <105808002+Sayed73@users.noreply.github.com>
2024-09-08 09:56:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -223,6 +223,12 @@ def main():
         # st.markdown(f'<p style="font-size:24px; color:black; font-weight:bold;">Diagnosis :</p>', unsafe_allow_html=True)
         st.markdown(f'<p style="font-size:22px; color:#2e6c80;"><strong>Pulpal diagnosis:</strong> {predicted_class_pulpal}  </p>', unsafe_allow_html=True)
         st.markdown(f'<p style="font-size:22px; color:#2e6c80;"><strong>Periapical diagnosis:</strong> {predicted_class_periapical}  </p>', unsafe_allow_html=True)
+        # Compare user input with predictions
+        if user_pulpal_diagnose == prediction_pulpal and user_periapical_diagnose == prediction_periapical:
+            st.success("Well done! Your diagnoses match the prediction.")
+            st.balloons()  # Optional: show balloons as a celebration
+        else:
+            st.warning("Your diagnosis does not match the predicted diagnosis.")
 
         # Save the record to S3
         # Convert features to a list of 'NA' if the value is 99
